08462f07f531d7e22ec5e652b745e24f8e849f20
Zivan <zivanmisquitta1@gmail.com>
2026-02-12 14:00:35 +0530

    Added category configuration map

---
+++ config.py
@@ -0,0 +1,15 @@
+# config.py
+
+# A map of "Human Name" -> "YouTube API Category ID"
+CATEGORY_MAP = {
+    "Gaming": "20",
+    "Technology": "28",  # Science & Tech
+    "Education": "27",
+    "Entertainment": "24",
+    "Music": "10",
+    "News": "25",  # News & Politics
+    "Comedy": "23",
+    "Sports": "17",
+    "Howto": "26",  # How-to & Style
+    "Film": "1",  # Film & Animation
+}
